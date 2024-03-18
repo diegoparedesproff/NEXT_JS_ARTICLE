7ddbd4d1c009155c73564dbfc1661f6824be2dc2
dparedesgorilla <diego.paredes@gorillalogic.com>
2024-03-18 11:44:50 -0500

    db tests

--- src/app/(components)/TaskList.tsx
+++ src/app/(components)/TaskList.tsx
@@ -1,25 +1,29 @@
-import TaskListItem from './TaskListItem';
-import path from 'path';
+import TaskListItem from "./TaskListItem";
+import path from "path";
 
-const fetchTasks = async ()=>{
+let myUrl = "esta";
+
+const fetchTasks = async () => {
   //const response = await fetch('http://localhost:3000/api/tasks');
   let data = [];
-  try{
+  try {
     const baseUrl = `https://${process.env.VERCEL_URL}`;
-    console.log("baseUrl ===== ",baseUrl)
-    const response = await fetch(baseUrl+'/db.json');
-    console.log("response = ",response);
+    console.log("baseUrl ===== ", baseUrl);
+    myUrl = baseUrl;
+    const response = await fetch(baseUrl + "/db.json");
+    console.log("response = ", response);
     data = await response.json();
-  }catch(err){
-    console.log("error en el fetch: ",err)
+  } catch (err) {
+    console.log("error en el fetch: ", err);
   }
   return data;
-}
+};
 
 const TaskList = async () => {
   const tasks = await fetchTasks();
   return (
     <ul className="taskList">
+      <div>{`La url = ${myUrl}`}</div>
       {tasks?.map((task) => (
         <TaskListItem key={task.id} task={task} />
       ))}

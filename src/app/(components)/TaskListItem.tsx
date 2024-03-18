60b8e940065d1601bd62073ff1368672b4d799f8
dparedesgorilla <diego.paredes@gorillalogic.com>
2024-03-18 16:11:39 -0500

    db tests

--- src/app/(components)/TaskListItem.tsx
+++ src/app/(components)/TaskListItem.tsx
@@ -1,14 +1,19 @@
-'use client'
+"use client";
 
 import { deleteTask, updateTask } from "../(actions)";
 
 const TaskListItem = ({ task }) => {
+  console.log("task = ", task);
   return (
-    <li className={task.completed ? 'completed' : ''}>
+    <li className={task.completed ? "completed" : ""}>
       <span>{task.content}</span>
       <div>
-        <input type="checkbox" defaultChecked={task.completed} onClick={()=>updateTask(task)} />
-        <button onClick={()=>deleteTask(task.id)}>x</button>
+        <input
+          type="checkbox"
+          defaultChecked={task.completed}
+          onClick={() => updateTask(task)}
+        />
+        <button onClick={() => deleteTask(task.id)}>x</button>
       </div>
     </li>
   );

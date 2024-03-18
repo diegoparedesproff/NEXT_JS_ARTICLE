1601add41fea808f46671c51b417a8afdf2a0bb3
dparedesgorilla <diego.paredes@gorillalogic.com>
2024-03-18 11:08:52 -0500

    db tests

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -5,12 +5,13 @@ const fs = require("fs");
 
 //This is a standard js request object
 export const GET = async (request: Request) => {
-  const data = getTasks();
+  const data = await getTasks();
+  console.log("data = ", data);
   return NextResponse.json(data);
 };
 
 export const POST = async (request: Request) => {
   const data = await request.json();
-  addTask(data.content)
-  return NextResponse.json({message:"success"});
+  addTask(data.content);
+  return NextResponse.json({ message: "success" });
 };

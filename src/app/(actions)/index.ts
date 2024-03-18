76824482e41998970f4ae56e6d52a9b9a184b83e
dparedesgorilla <diego.paredes@gorillalogic.com>
2024-03-18 14:04:45 -0500

    db tests

--- src/app/(actions)/index.ts
+++ src/app/(actions)/index.ts
@@ -8,23 +8,6 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const filePath = process.cwd() + "src/app/(fakeDB)/index.json";
 
-/*
-export const getTasks = () => {
-  try {
-    if (fs.existsSync(filePath)) {
-      const jsonData = fs.readFileSync(filePath, "utf8");
-      const fakeDB = JSON.parse(jsonData);
-      return fakeDB;
-    } else {
-      fs.writeFileSync(filePath, [], "utf8");
-      return [];
-    }
-  } catch (error) {
-    console.log("filePath = ", console.log(filePath));
-    return error;
-  }
-};*/
-
 export const getTasks = async () => {
   try {
     const { rows } = await sql`SELECT * from tasks ORDER BY createdAt`;
@@ -42,27 +25,14 @@ const saveToFakeDB = (payload) => {
   fs.writeFileSync(filePath, newJsonData, "utf8");
 };
 
-/*
-export const addTask = (props: FormData | string) => {
-  const content = (typeof props === "string") ? props : props.get("task");
-  const id = uuidv4();
-  const createdAt = Date.now();
-  const newTask = { id, content, completed: false, createdAt };
-  const newTaskList = [...getTasks(), newTask];
-  saveToFakeDB(newTaskList);
-  revalidatePath("/tasks");
-};
-*/
-
 export const addTask = async (props: FormData | string) => {
   const content = typeof props === "string" ? props : props.get("task");
   const { rows } = await sql`INSERT INTO task (content) VALUES (${content})`;
   revalidatePath("/tasks");
 };
 
-export const deleteTask = (id) => {
-  const newTaskList = getTasks().filter((task) => task.id !== id);
-  saveToFakeDB(newTaskList);
+export const deleteTask = async (id) => {
+  const newTaskList = await `DELETE FROM tasks WHERE id = ${id}`;
   revalidatePath("/tasks");
 };
 
